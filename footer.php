b14fdab1511825a448247777e4848859fbb71df1
Michael Dillon <dillon@protoforge.com>
2017-03-19 14:09:27 -0600

    formatted copyright

--- footer.php
+++ footer.php
@@ -18,8 +18,7 @@
 				<div class="row">
 					<?php if( !get_theme_mod('footer_social') ) activello_social_icons(); ?>
 					<div class="copyright col-md-12">
-						<?php echo esc_html( get_theme_mod( 'activello_footer_copyright', 'Activello' ) ); ?>
-						<?php activello_footer_info(); ?>
+						&copy; <?php echo date("Y"); ?> <?php echo esc_html( get_theme_mod( 'activello_footer_copyright', 'Activello' ) ); ?>
 					</div>
 				</div>
 			</div><!-- .site-info -->
